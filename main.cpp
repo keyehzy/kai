e087b33b2cea44f86767959b0361d40cc8a818e6
msousa <msousa@sjtu.edu.cn>
2025-06-04 15:21:54 +0800

    Add basic instruction for the bytecode

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ class Bytecode {
   typedef u64 Value;
   typedef u64 Label;
 
-  struct BasicBlock;;
+  struct BasicBlock;
   std::vector<BasicBlock> blocks;
 
   u64 register_count = 0;
@@ -61,6 +61,57 @@ struct Bytecode::Instruction {
 protected:
   Instruction(Type type) : type(type) {}
 };
+
+struct Bytecode::Instruction::Move final : Bytecode::Instruction {
+  Move(Register dst, Register src) : Bytecode::Instruction(Type::Move), dst(dst), src(src) {}
+
+  Register dst;
+  Register src;
+};
+
+struct Bytecode::Instruction::Load final : Bytecode::Instruction {
+  Load(Register dst, Value value) : Bytecode::Instruction(Type::Load), dst(dst), value(value) {}
+
+  Register dst;
+  Value value;
+};
+
+struct Bytecode::Instruction::LessThan final : Bytecode::Instruction {
+  LessThan(Register dst, Register lhs, Register rhs) : Bytecode::Instruction(Type::LessThan), dst(dst), lhs(lhs), rhs(rhs) {}
+
+  Register dst;
+  Register lhs;
+  Register rhs;
+};
+
+struct Bytecode::Instruction::Jump final : Bytecode::Instruction {
+  Jump(Label label) : Bytecode::Instruction(Type::Jump), label(label) {}
+
+  Label label;
+};
+
+struct Bytecode::Instruction::JumpConditional final : Bytecode::Instruction {
+  JumpConditional(Register cond, Label then, Label else_) : Bytecode::Instruction(Type::JumpConditional), cond(cond), then(then), else_(else_) {}
+
+  Register cond;
+  Label then;
+  Label else_;
+};
+
+struct Bytecode::Instruction::Return final : Bytecode::Instruction {
+  Return(Register reg) : Bytecode::Instruction(Type::Return), reg(reg) {}
+
+  Register reg;
+};
+
+struct Bytecode::Instruction::Add final : Bytecode::Instruction {
+  Add(Register dst, Register src, Value value) : Bytecode::Instruction(Type::Add), dst(dst), src(src), value(value) {}
+
+  Register dst;
+  Register src;
+  Value value;
+};
+
 
 struct Bytecode::BasicBlock {
   std::vector<std::unique_ptr<Instruction>> instructions;
