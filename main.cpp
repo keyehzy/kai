3a7bd50d716ff601d0022f631731c6c3159d1a3a
msousa <msousa@sjtu.edu.cn>
2025-06-04 15:10:20 +0800

    Instroduce Bytecode

--- main.cpp
+++ main.cpp
@@ -13,46 +13,67 @@
 //   return t1;
 // }
 
-#include "ast.h"
+#include <cstdint>
+#include <vector>
+#include <memory>
 
-Ast::FunctionDeclaration fibonacci_example() {
-  // begin function declaration
-  auto function_decl =
-      std::make_unique<Ast::FunctionDeclaration>("foo", std::make_unique<Ast::Block>());
-  auto &decl_body = function_decl->body;
+typedef uint64_t u64;
 
-  // declare n, i, t1, t2, t3
-  decl_body->append<Ast::VariableDeclaration>("n", std::make_unique<Ast::Literal>(10));
-  decl_body->append<Ast::VariableDeclaration>("t1", std::make_unique<Ast::Literal>(0));
-  decl_body->append<Ast::VariableDeclaration>("t2", std::make_unique<Ast::Literal>(1));
-  decl_body->append<Ast::VariableDeclaration>("t3", std::make_unique<Ast::Literal>(0));
+class Bytecode {
 
-  // for (int i = 0; i < n; i++) {
-  //   t3 = t1 + t2;
-  //   t1 = t2;
-  //   t2 = t3;
-  // }
-  decl_body->append<Ast::VariableDeclaration>("i", std::make_unique<Ast::Literal>(0));
-  auto less_than =
-      std::make_unique<Ast::LessThan>(std::make_unique<Ast::Variable>("i"), std::make_unique<Ast::Variable>("n"));
-  auto block = std::make_unique<Ast::Block>();
-  block->append<Ast::Assignment>(
-      "t3", std::make_unique<Ast::Add>(std::make_unique<Ast::Variable>("t1"), std::make_unique<Ast::Variable>("t2")));
-  block->append<Ast::Assignment>("t1", std::make_unique<Ast::Variable>("t2"));
-  block->append<Ast::Assignment>("t2", std::make_unique<Ast::Variable>("t3"));
-  block->append<Ast::Increment>(std::make_unique<Ast::Variable>("i"));
-  auto while_loop = std::make_unique<Ast::While>(std::move(less_than), std::move(block));
-  decl_body->children.push_back(std::move(while_loop));
-  // end for loop
+  struct Instruction;
 
-  decl_body->append<Ast::Return>(std::make_unique<Ast::Variable>("t1"));
-  // end function declaration
-  return std::move(*function_decl);
-}
+  typedef u64 Register;
+  typedef u64 Value;
+  typedef u64 Label;
+
+  struct BasicBlock;;
+  std::vector<BasicBlock> blocks;
+
+  u64 register_count = 0;
+  Register allocate_register() { return register_count++; }
+  Register current_register() { return register_count - 1; }
+};
+
+struct Bytecode::Instruction {
+  enum class Type {
+    Move,
+    Load,
+    LessThan,
+    Jump,
+    JumpConditional,
+    Return,
+    AddImmediate,
+  };
+
+  Type type;
+
+  struct Move;
+  struct Load;
+  struct LessThan;
+  struct Jump;
+  struct JumpConditional;
+  struct Return;
+  struct Add;
+
+  virtual ~Instruction() = default;
+
+protected:
+  Instruction(Type type) : type(type) {}
+};
+
+struct Bytecode::BasicBlock {
+  std::vector<std::unique_ptr<Instruction>> instructions;
+
+  template <typename T, typename... Args>
+  T& append(Args&& ...args) {
+    instructions.emplace_back(std::make_unique<T>(std::forward<Args>(args)...));
+    return static_cast<T&>(*instructions.back());
+  }
+};
+
 
 
 int main() {
-  auto function_decl = fibonacci_example();
-  function_decl.to_string(std::cout);
-  std::cout << AstInterpreter().interpret(function_decl) << std::endl;
+  return 0;
 }
