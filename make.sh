effb487661a05a488e596ebdfec6669af59cf6f8
Matheus Sousa <msousa@sjtu.edu.cn>
2026-02-22 19:47:33 +0800

    fix linker

--- make.sh
+++ make.sh
@@ -4,8 +4,9 @@ set -e
 set -x
 set -o pipefail
 
-CXXFLAGS="-O0 -g3 -Wall -Wextra -std=c++20"
-CXX="/opt/homebrew/opt/llvm/bin/clang++"
-CC="/opt/homebrew/opt/llvm/bin/clang"
+CXXFLAGS="${CXXFLAGS:--O0 -g3 -Wall -Wextra -std=c++20}"
+CXX="${CXX:-clang++}"
+CC="${CC:-clang}"
 
 ${CXX} ${CXXFLAGS} -o main main.cpp ast.cpp
+./main
