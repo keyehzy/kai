2f6b35070949726f2af976b1fc23aadc8607891f
Matheus Sousa <msousa@sjtu.edu.cn>
2026-02-22 20:38:53 +0800

    Inline AST examples as local lambdas in tests

--- main.cpp
+++ main.cpp
@@ -47,77 +47,26 @@ std::unique_ptr<Ast::IfElse> if_else(std::unique_ptr<Ast::LessThan> condition,
                                        std::move(else_body));
 }
 
-Ast::Block fibonacci_example() {
-  // begin function declaration
-  auto decl_body = std::make_unique<Ast::Block>();
-
-  // declare n, i, t1, t2, t3
-  decl_body->append(decl("n", lit(10)));
-  decl_body->append(decl("t1", lit(0)));
-  decl_body->append(decl("t2", lit(1)));
-  decl_body->append(decl("t3", lit(0)));
-
-  // for (int i = 0; i < n; i++) {
-  //   t3 = t1 + t2;
-  //   t1 = t2;
-  //   t2 = t3;
-  // }
-  decl_body->append(decl("i", lit(0)));
-  auto block = std::make_unique<Ast::Block>();
-  block->append(assign("t3", add(var("t1"), var("t2"))));
-  block->append(assign("t1", var("t2")));
-  block->append(assign("t2", var("t3")));
-  block->append(inc("i"));
-  decl_body->append(while_loop(lt(var("i"), var("n")), std::move(block)));
-  // end for loop
-
-  decl_body->append(ret(var("t1")));
-  // end function declaration
-  return std::move(*decl_body);
-}
-
-Ast::Block factorial_example() {
-  auto decl_body = std::make_unique<Ast::Block>();
-  decl_body->append(decl("n", lit(5)));
-  decl_body->append(decl("result", lit(1)));
-  decl_body->append(decl("i", lit(1)));
-
-  auto block = std::make_unique<Ast::Block>();
-  block->append(assign("result", mul(var("result"), var("i"))));
-  block->append(inc("i"));
-  decl_body->append(while_loop(lt(var("i"), add(var("n"), lit(1))), std::move(block)));
-  decl_body->append(ret(var("result")));
-  return std::move(*decl_body);
-}
-
-Ast::Block max_example() {
-  auto decl_body = std::make_unique<Ast::Block>();
-  decl_body->append(decl("a", lit(4)));
-  decl_body->append(decl("b", lit(7)));
-  decl_body->append(decl("max", lit(0)));
-
-  auto if_body = std::make_unique<Ast::Block>();
-  if_body->append(assign("max", var("a")));
-
-  auto else_body = std::make_unique<Ast::Block>();
-  else_body->append(assign("max", var("b")));
-
-  decl_body->append(
-      if_else(lt(var("b"), var("a")), std::move(if_body), std::move(else_body)));
-  decl_body->append(ret(var("max")));
-  return std::move(*decl_body);
-}
-
-std::unique_ptr<Ast::FunctionDeclaration> function_example() {
-  auto decl_body = std::make_unique<Ast::Block>();
-  decl_body->append(decl("a", lit(4)));
-  decl_body->append(decl("b", lit(2)));
-  decl_body->append(ret(add(var("a"), var("b"))));
-  return std::make_unique<Ast::FunctionDeclaration>("sum", std::move(decl_body));
-}
-
 void test_fibonacci() {
-  auto fib = fibonacci_example();
+  auto fib = [] {
+    auto decl_body = std::make_unique<Ast::Block>();
+
+    decl_body->append(decl("n", lit(10)));
+    decl_body->append(decl("t1", lit(0)));
+    decl_body->append(decl("t2", lit(1)));
+    decl_body->append(decl("t3", lit(0)));
+
+    decl_body->append(decl("i", lit(0)));
+    auto block = std::make_unique<Ast::Block>();
+    block->append(assign("t3", add(var("t1"), var("t2"))));
+    block->append(assign("t1", var("t2")));
+    block->append(assign("t2", var("t3")));
+    block->append(inc("i"));
+    decl_body->append(while_loop(lt(var("i"), var("n")), std::move(block)));
+
+    decl_body->append(ret(var("t1")));
+    return std::move(*decl_body);
+  }();
   kai::bytecode::BytecodeGenerator gen;
   gen.visit_block(fib);
   gen.finalize();
@@ -128,7 +77,19 @@ void test_fibonacci() {
 }
 
 void test_factorial() {
-  auto factorial = factorial_example();
+  auto factorial = [] {
+    auto decl_body = std::make_unique<Ast::Block>();
+    decl_body->append(decl("n", lit(5)));
+    decl_body->append(decl("result", lit(1)));
+    decl_body->append(decl("i", lit(1)));
+
+    auto block = std::make_unique<Ast::Block>();
+    block->append(assign("result", mul(var("result"), var("i"))));
+    block->append(inc("i"));
+    decl_body->append(while_loop(lt(var("i"), add(var("n"), lit(1))), std::move(block)));
+    decl_body->append(ret(var("result")));
+    return std::move(*decl_body);
+  }();
   kai::bytecode::BytecodeGenerator gen;
   gen.visit_block(factorial);
   gen.finalize();
@@ -139,7 +100,13 @@ void test_factorial() {
 }
 
 void test_function_declaration() {
-  auto factorial = function_example();
+  auto factorial = [] {
+    auto decl_body = std::make_unique<Ast::Block>();
+    decl_body->append(decl("a", lit(4)));
+    decl_body->append(decl("b", lit(2)));
+    decl_body->append(ret(add(var("a"), var("b"))));
+    return std::make_unique<Ast::FunctionDeclaration>("sum", std::move(decl_body));
+  }();
   kai::bytecode::BytecodeGenerator gen;
   gen.visit(*factorial);
   gen.finalize();
@@ -150,7 +117,23 @@ void test_function_declaration() {
 }
 
 void test_if_else() {
-  auto max = max_example();
+  auto max = [] {
+    auto decl_body = std::make_unique<Ast::Block>();
+    decl_body->append(decl("a", lit(4)));
+    decl_body->append(decl("b", lit(7)));
+    decl_body->append(decl("max", lit(0)));
+
+    auto if_body = std::make_unique<Ast::Block>();
+    if_body->append(assign("max", var("a")));
+
+    auto else_body = std::make_unique<Ast::Block>();
+    else_body->append(assign("max", var("b")));
+
+    decl_body->append(
+        if_else(lt(var("b"), var("a")), std::move(if_body), std::move(else_body)));
+    decl_body->append(ret(var("max")));
+    return std::move(*decl_body);
+  }();
   kai::bytecode::BytecodeGenerator gen;
   gen.visit_block(max);
   gen.finalize();
