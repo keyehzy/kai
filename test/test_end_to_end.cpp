f207f2552f8f549967c9db0d3efd7dfeb388d913
Matheus Sousa <msousa@sjtu.edu.cn>
2026-02-23 13:40:52 +0800

    Add parser end-to-end expression pipeline test

---
+++ test/test_end_to_end.cpp
@@ -0,0 +1,24 @@
+#include "../ast.h"
+#include "../bytecode.h"
+#include "../catch.hpp"
+#include "../parser.h"
+
+TEST_CASE("test_parser_expression_end_to_end_literal_42") {
+  kai::Parser parser("42");
+  std::unique_ptr<kai::ast::Ast> parsed_expression = parser.parse_expression();
+
+  REQUIRE(parsed_expression != nullptr);
+
+  kai::ast::AstInterpreter ast_interpreter;
+  REQUIRE(ast_interpreter.interpret(*parsed_expression) == 42);
+
+  kai::ast::Ast::Block program;
+  program.append(std::make_unique<kai::ast::Ast::Return>(std::move(parsed_expression)));
+
+  kai::bytecode::BytecodeGenerator generator;
+  generator.visit_block(program);
+  generator.finalize();
+
+  kai::bytecode::BytecodeInterpreter bytecode_interpreter;
+  REQUIRE(bytecode_interpreter.interpret(generator.blocks()) == 42);
+}
